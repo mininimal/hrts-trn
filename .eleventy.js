5cdc7e6906a6552b75e88982e1656aa7b24aff70
gmgmgm <gm@gmgmgm.design>
2026-07-28 12:11:21 -0700

    Removes giant titles from Values cards

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,18 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const sitemapPlugin = require("@quasibit/eleventy-plugin-sitemap");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+  eleventyConfig.addPlugin(sitemapPlugin, {
+    sitemap: {
+      hostname: "https://heartsturnhealing.com",
+    },
+  });
 
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/robots.txt");
 
   return {
     dir: {
